004f9bf82384d3c6ce709d75b62548d274ed401f
Sam Tay <sam.chong.tay@gmail.com>
2016-07-22 09:55:55 -0400

    Fix m2 auto login

--- tamper-monkey/magento2-auto-login.js
+++ tamper-monkey/magento2-auto-login.js
@@ -7,50 +7,62 @@
 // @match        *.dev/*admin*
 // ==/UserScript==
 
-window.onload = function() {
-  if(typeof jQuery !== "undefined") {
-    (function($) {
-      if ($('body.adminhtml-auth-login').length) {
-        try {
-          showPopup($);
-        } catch(err) {
-          console.log(err);
-        }
-        $('#username').val('samtay');
-        $('#login').val('matrix7');
-        $('#login-form').submit();
-      }
-    })(jQuery);
-  }
-};
+// Credentials
+var username = 'admin',
+    password = 'pass4admin';
 
+// On load, wait for jQuery to come on board, then login
+timer();
+function timer() {
+  if (typeof jQuery === "function") {
+    login(jQuery);
+  } else {
+    setTimeout(timer, 200);
+  }
+}
+
+// Pass login credentials and submit form
+function login($) {
+  if ($('body.adminhtml-auth-login').length) {
+    try {
+      showPopup($);
+    } catch(err) {
+      console.log(err);
+    }
+    $('#username').val('admin');
+    $('#login').val('pass4admin');
+    $('#login-form').submit();
+  }
+}
+
+// Pop up for tamper monkey, not very important
 function showPopup($){
   var docHeight = $(window).width();
 
   $("body").append("<div id='overlay'></div><div id='login-message'>Logging in with Tampermonkey...</div>");
 
   $("#overlay")
-      .css({
-        'height' : docHeight + 'px',
-        'opacity' : 0.4,
-        'position': 'absolute',
-        'top': 0,
-        'left': 0,
-        'backgroundColor': 'black',
-        'width': '100%',
-        'zIndex': 5000
-      });
+    .css({
+      'height' : docHeight + 'px',
+      'opacity' : 0.4,
+      'position': 'absolute',
+      'top': 0,
+      'left': 0,
+      'backgroundColor': 'black',
+      'width': '100%',
+      'zIndex': 5000
+    });
 
   $("#login-message")
-      .css({
-        'height' : '80px',
-        'position': 'absolute',
-        'backgroundColor': 'white',
-        'width': '200px',
-        'zIndex': 5001,
-        'padding': '20px',
-        'border': '5px solid black'
-      });
+    .css({
+      'height' : '80px',
+      'position': 'absolute',
+      'backgroundColor': 'white',
+      'width': '200px',
+      'zIndex': 5001,
+      'padding': '20px',
+      'border': '5px solid black'
+    });
 
   var vpWidth = $(window).width();
   var width = $("#login-message").width();
@@ -63,8 +75,8 @@ function showPopup($){
   var avTop = (vpHeight / 2) - (height / 2) + scrollTop + 'px';
 
   $("#login-message")
-      .css({
-        'top': avTop,
-        'left': avLeft
-      });
+    .css({
+      'top': avTop,
+      'left': avLeft
+    });
 }
